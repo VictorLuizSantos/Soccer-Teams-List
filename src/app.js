65ac626acea913915a5ca0c37695387ab0ae7686
VICTOR LUIZ MENDONÇA DOS SANTOS <2022001518@ifam.edu.br>
2024-08-24 10:31:49 -0400

    Implementacao CRUD na lista de selecoes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express()
+app.use(express.json())
+
+const selecoes = [
+    {id: 1, selecao: "Argentina", grupo: "A"},
+    {id: 2, selecao: "Brasil", grupo: "B"},
+    {id: 3, selecao: "Chile", grupo: "C"},
+    {id: 4, selecao: "Dinamarca", grupo: "D"},
+    {id: 5, selecao: "Equador", grupo: "E"}
+]
+
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id)
+}
 
 app.get('/', (req, res) => {
     res.send('Pegando dados...')
+})
+
+app.get('/selecoes', (req, res) => {
+    res.send(selecoes)
+})
+
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send('Seleção adicionada com sucesso')
+})
+
+app.put('/selecoes/:id',(req, res) => {
+    const index = req.params.id - 1
+     selecoes[index] = req.body
+     res.send(`Selecao com id ${req.params.id} alterada com sucesso`)
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    const index = req.params.id
+    console.log(index)
+    selecoes.splice(index - 1, 1)
+    res.send(`Selecao com id ${req.params.id} excluida com sucesso`)
 })
 
 export default app
